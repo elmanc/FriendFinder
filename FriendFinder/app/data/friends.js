06161a9c5e66d5f155f391d64095a22ce34bf9e9
elmanc <elmancole@hotmail.com>
2018-02-08 18:30:25 -0500

    Update friends.js

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,43 +1,39 @@
-// @author: Thomas Thompson
-// @github: tomtom28
-// @comment: Homework 13 - Friend Finder Express Node app
 
-
-// Templated Friends Data
+// Friends Data
 var friendsData = [
   {
-    name: "Tommy Thompson",
-    photo: "https://avatars1.githubusercontent.com/u/17932981?v=3&s=466",
+    name: "Drake",
+    photo: "http://static1.businessinsider.com/image/56e34aad52bcd022008b5fa5-1300/drizzy.jpg",
     scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
   },
   {
-    name: "Number 2",
-    photo:"https://upload.wikimedia.org/wikipedia/en/thumb/e/e5/No_2_APTSWSM.jpg/200px-No_2_APTSWSM.jpg",
+    name: "Prince",
+    photo:"https://www.pbs.org/newshour/app/uploads/2016/04/GettyImages-85842490.jpg",
     scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
   },
   {
-    name: "The One",
-    photo:"https://s-media-cache-ak0.pinimg.com/564x/dd/31/d2/dd31d2dd156204fcf9b6aa1c54455adb.jpg",
+    name: "Taylor Swift",
+    photo:"http://static3.businessinsider.com/image/56818511c08a8037008b53ce-480/taylor-swift.jpg",
     scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   },
   {
-    name: "Trinity",
-    photo:"http://vignette3.wikia.nocookie.net/matrix/images/6/67/Trinityfull.jpg/revision/latest?cb=20060803214449",
+    name: "Beyonce",
+    photo:"https://www.thewrap.com/wp-content/uploads/2013/12/beyonce-itunes-record.jpg",
     scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
   },
   {
-    name: "Dr. Evil",
-    photo:"https://at-cdn-s01.audiotool.com/2012/05/28/documents/ei368KjdlP5qy6gILDLtH9cEAf6H/0/cover256x256-b31436c2a9bc4645b1ad67bc09705cd7.jpg",
+    name: "Elton John",
+    photo:"https://www.thewrap.com/wp-content/uploads/2014/12/elton-john-world-aids-day.jpg",
     scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
   },
   {
-    name: "Mia Wallace",
-    photo:"https://s-media-cache-ak0.pinimg.com/originals/a9/01/29/a9012982eea651ee31e21f4ad11fe96b.jpg",
+    name: "JayZ",
+    photo:"https://www.thewrap.com/wp-content/uploads/2014/10/jay-z.jpg",
     scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
   },
   {
-    name: "Princess Leia",
-    photo:"http://images.techtimes.com/data/images/full/168082/princess-leia-organa.jpg?w=600",
+    name: "Madonna",
+    photo:"https://www.thewrap.com/wp-content/uploads/2015/01/madonna-rebel-heart-150112.jpeg",
     scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
   }
 ];
